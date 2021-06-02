766dfde9baeafae927df1ef2fb9e146f449e5ebe
zein rezky chandra <zeinrezky@gmail.com>
2021-06-02 09:11:54 +0700

    Initial Commit

---
+++ mentor-spaceship/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mentor-spaceship
+//
+//  Created by zein rezky chandra on 02/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
